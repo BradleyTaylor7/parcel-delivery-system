5e1081d97f9b05efff9dfdc1643e89319463ac54
Bradley Taylor <bradley.taylor07@gmail.com>
2021-02-03 21:23:17 +0000

    All files

---
+++ Unit 15 OOP - Assignment 3/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_15_OOP___Assignment_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const float regdelivery1 = 7.15f, standard1 = 5.00f, standard2 = 2.20f, recordeddelivery = 6.20f, xregdelivery1 = 2.00f, xstandard1 = 0.50f, xstandard2 = 0.20f, xrecordeddelivery = 1.00f;
+            float userweight = 0, realweight = 0;
+            string CustomerName;
+
+            //showing the prices
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* Type of service  *Basic charge for 1KG (£)* Extra charge per unit weight (£) *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("*Registered        *         7.15           *               2.00               *");
+            Console.WriteLine("*delivery 1st Class*                        *                                  *");
+            Console.WriteLine("*Standard          *         5.00           *               0.50               *");
+            Console.WriteLine("*1st Class         *                        *                                  *");
+            Console.WriteLine("*Standard          *         2.20           *               0.20               *");
+            Console.WriteLine("*2nd Class         *                        *                                  *");
+            Console.WriteLine("*48 hour           *         6.20           *               1.00               *");
+            Console.WriteLine("*recorded delivery *                        *                                  *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("*Press enter to continue with your delivery quote...                           *");
+            Console.WriteLine("********************************************************************************");
+            Console.ReadKey();
+
+            //Enter customer details
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("Please enter your name");
+            CustomerName = Console.ReadLine();
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("Please enter your Email");
+            Console.ReadLine();
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("Please enter your mobile number");
+            Console.ReadLine();
+
+            //Asking for the weight of the parcel
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("What is the weight of your parcel in KG?");
+            realweight = float.Parse(Console.ReadLine());
+
+            //Customers name message
+            Console.WriteLine("Here is your personal delivery quote " + CustomerName);
+            
+            //IF statements to work out the prices for each delivery type
+
+            //registered delivery first class if statement
+            if (realweight <= 1) {
+                Console.WriteLine("The price for send your parcel through registered delivery 1st class is £" + regdelivery1);
+            } else if (realweight > 1) {
+                userweight = realweight - 1;
+                float price = (userweight * xregdelivery1 + regdelivery1);
+                Console.WriteLine("The price for send your parcel through registered delivery 1st class is £" + price);
+            }
+            
+            //registered standard first class if statement
+            if (realweight <= 1)
+            {
+                Console.WriteLine("The price for send your parcel through standard 1st class is £" + standard1);
+            }
+            else if (realweight > 1)
+            {
+                userweight = realweight - 1;
+                float price = (userweight * xstandard1 + standard1);
+                Console.WriteLine("The price for send your parcel through standard 1st class is £" + price);
+            }
+
+            //stantard second class if statement
+            if (realweight <= 1)
+            {
+                Console.WriteLine("The price for send your parcel through standard 2nd class is £" + standard2);
+            }
+            else if (realweight > 1)
+            {
+                userweight = realweight - 1;
+                float price = (userweight * xstandard2 + standard2);
+                Console.WriteLine("The price for send your parcel through standard 2nd class is £" + price);
+            }
+
+            //48 hour recorded delivery if statement
+            if (realweight <= 1)
+            {
+                Console.WriteLine("The price for send your parcel through 48 hour delivery is £" + recordeddelivery);
+            }
+            else if (realweight > 1)
+            {
+                userweight = realweight - 1;
+                float price = (userweight * xrecordeddelivery + recordeddelivery);
+                Console.WriteLine("The price for send your parcel through 48 hour delivery is £" + price);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Unit 15 OOP - Assignment 3/PseudoCode.cs
@@ -0,0 +1,26 @@
+﻿
+//Unit 15 Object Oriented Programming - Assignment 2
+//Author - Bradley Taylor
+
+//Declare variables as i will need variable to store data such as the customers name, prices and fees etc
+//Will need parameter such as float and string
+
+//Here i will want to show the prices on screen for the customer to see
+//I will have to use console writeline and readkey to show the prices and extra fees
+
+//Then i will need the customers details
+//I will ask for the customers name and use readline to store what they tpye into the application
+//I will ask for the customers email address
+//I will ask for the customers mobile number
+
+//Here i will gain the information such as the weight of the parcel which the customer will type in 
+
+//I will make a message saying here is your quote and add the customer name to the end
+
+//I will need to add IF statements which will be used to calculate the price for the customer depending on the weight of the parcel which they entered in
+//I will use a IF ELSE statements
+
+//IF the weight is equal to or less than 1kg then the cost for the customer will be the original price with no extra fees
+//ELSE IF the weight is more than 1kg then the cost for the customer will be the original price plus an extra fee depending on how many kgs over the parcel is
+
+//These statments will be used to work out all of the prices for each delivery option so the customer can see what is the most and least expensive
